0bc796cd0dbf88f20f4f37d5cefa8398c44aa7dc
Andreea MacBook <andreeamacbook@Andreeas-MacBook.local>
2023-12-20 17:51:37 +0200

    form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { PortofoliuComponent } from './portofoliu/portofoliu.component';
+import { FormComponent } from './form/form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, PortofoliuComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, PortofoliuComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
