3daef7c76ff31fbefc0f93dda4ff2c10f28e9f12
Andreea MacBook <andreeamacbook@Andreeas-MacBook.local>
2024-01-05 16:51:13 +0200

    redirectionare siteuri

--- frontend/src/app/portofoliu/portofoliu.component.ts
+++ frontend/src/app/portofoliu/portofoliu.component.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { Portofoliu } from '../portofoliu';
 import {PortofoliiService} from '../portofolii.service';
+
 @Component({
   selector: 'app-portofoliu',
   standalone: true,
@@ -17,5 +18,7 @@ portofoliiService:PortofoliiService = inject(PortofoliiService);
 constructor() {
   this.portofolii = this.portofoliiService.getAllPortofolios();
 }
-
+goToUrl(url:any):void{
+  window.location.href=url;
+}
 }
